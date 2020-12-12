370c9dfb2726328922a69cb94f8258a10530bcea
Bruno Santos <brunorene@gmail.com>
2020-12-12 22:25:41 +0000

    day 9 part 1

--- day09/main.go
+++ day09/main.go
@@ -46,7 +46,7 @@ func (p *program) read(address int64) int64 {
 	return value
 }
 
-func (p *program) readWithMode(mode int64, offset int64) int64 {
+func (p *program) get(mode int64, offset int64) int64 {
 	switch mode {
 	case 1:
 		return p.read(p.pointer + offset)
@@ -57,7 +57,7 @@ func (p *program) readWithMode(mode int64, offset int64) int64 {
 	}
 }
 
-func (p *program) writeWithMode(mode int64, offset int64, value int64) {
+func (p *program) set(mode int64, offset int64, value int64) {
 	switch mode {
 	case 2:
 		p.code[p.read(p.pointer+offset)+p.relativeBase] = value
@@ -66,90 +66,58 @@ func (p *program) writeWithMode(mode int64, offset int64, value int64) {
 	}
 }
 
-func runProgram(program program, input []int64) program {
-	for program.read(program.pointer) != 99 {
-		opcode := program.read(program.pointer) % 100
-		param1Mode := (program.read(program.pointer) / 100) % 10
-		param2Mode := (program.read(program.pointer) / 1000) % 10
-		param3Mode := (program.read(program.pointer) / 10000) % 10
-		log.Println("internal", program.read(program.pointer), param3Mode, param2Mode, param1Mode)
+func runProgram(prog program, input []int64) program {
+	for prog.read(prog.pointer) != 99 {
+		opcode := prog.read(prog.pointer) % 100
+		param1Mode := (prog.read(prog.pointer) / 100) % 10
+		param2Mode := (prog.read(prog.pointer) / 1000) % 10
+		param3Mode := (prog.read(prog.pointer) / 10000) % 10
 		switch opcode {
 		case 1: // sum
-			val1 := program.readWithMode(param1Mode, 1)
-			val2 := program.readWithMode(param2Mode, 2)
-
-			program.writeWithMode(param3Mode, 3, val1+val2)
-			program.pointer += 4
+			prog.set(param3Mode, 3, prog.get(param1Mode, 1)+prog.get(param2Mode, 2))
+			prog.pointer += 4
 		case 2: // mul
-			val1 := program.readWithMode(param1Mode, 1)
-			val2 := program.readWithMode(param2Mode, 2)
-
-			program.writeWithMode(param3Mode, 3, val1*val2)
-			program.pointer += 4
+			prog.set(param3Mode, 3, prog.get(param1Mode, 1)*prog.get(param2Mode, 2))
+			prog.pointer += 4
 		case 3: // input
-			position := program.readWithMode(param1Mode, 1)
-			value := input[0]
+			prog.set(param1Mode, 1, input[0])
 			input = input[1:]
-			if param3Mode == 2 {
-				program.code[position+program.relativeBase] = value
-			} else {
-				program.code[position] = value
-			}
-			program.pointer += 2
+			prog.pointer += 2
 		case 4: // ouput
-			position := program.readWithMode(param1Mode, 1)
-			switch param1Mode {
-			case 1: // immediate
-				log.Println(position)
-			case 2: // relative
-				log.Println(program.read(position + program.relativeBase))
-			default:
-				log.Println(program.read(position))
-			}
-			program.pointer += 2
+			log.Println(prog.get(param1Mode, 1))
+			prog.pointer += 2
 		case 5: // is true
-			val1 := program.readWithMode(param1Mode, 1)
-			val2 := program.readWithMode(param2Mode, 2)
-
-			if val1 != 0 {
-				program.pointer = val2
+			if prog.get(param1Mode, 1) != 0 {
+				prog.pointer = prog.get(param2Mode, 2)
 			} else {
-				program.pointer += 3
+				prog.pointer += 3
 			}
 		case 6: // is false
-			val1 := program.readWithMode(param1Mode, 1)
-			val2 := program.readWithMode(param2Mode, 2)
-
-			if val1 == 0 {
-				program.pointer = val2
+			if prog.get(param1Mode, 1) == 0 {
+				prog.pointer = prog.get(param2Mode, 2)
 			} else {
-				program.pointer += 3
+				prog.pointer += 3
 			}
 		case 7: // less than
-			val1 := program.readWithMode(param1Mode, 1)
-			val2 := program.readWithMode(param2Mode, 2)
-
-			program.writeWithMode(param3Mode, 3, 0)
-			if val1 < val2 {
-				program.writeWithMode(param3Mode, 3, 1)
+			if prog.get(param1Mode, 1) < prog.get(param2Mode, 2) {
+				prog.set(param3Mode, 3, 1)
+			} else {
+				prog.set(param3Mode, 3, 0)
 			}
-			program.pointer += 4
+			prog.pointer += 4
 		case 8: // equals
-			val1 := program.readWithMode(param1Mode, 1)
-			val2 := program.readWithMode(param2Mode, 2)
-
-			program.writeWithMode(param3Mode, 3, 0)
-			if val1 == val2 {
-				program.writeWithMode(param3Mode, 3, 1)
+			if prog.get(param1Mode, 1) == prog.get(param2Mode, 2) {
+				prog.set(param3Mode, 3, 1)
+			} else {
+				prog.set(param3Mode, 3, 0)
 			}
-			program.pointer += 4
+			prog.pointer += 4
 		case 9: // set relative base
-			val1 := program.readWithMode(param1Mode, 1)
-			program.relativeBase += val1
-			program.pointer += 2
+			prog.relativeBase += prog.get(param1Mode, 1)
+			prog.pointer += 2
 		}
 	}
-	return program
+	return prog
 }
 
 func main() {
